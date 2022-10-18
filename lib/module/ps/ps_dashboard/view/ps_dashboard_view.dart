b2b8f801388d6866d505d2cb01528da96aba41f6
Deny <flutterlabz@gmail.com>
2022-10-18 16:25:54 +0700

    .

--- lib/module/ps/ps_dashboard/view/ps_dashboard_view.dart
+++ lib/module/ps/ps_dashboard/view/ps_dashboard_view.dart
@@ -1,5 +1,6 @@
+import 'package:fhe_template/core.dart';
+import 'package:fhe_template/shared/widget/textfield/remote_autocomplete.dart';
 import 'package:flutter/material.dart';
-import '../controller/ps_dashboard_controller.dart';
 
 class PsDashboardView extends StatefulWidget {
   const PsDashboardView({Key? key}) : super(key: key);
@@ -15,10 +16,35 @@ class PsDashboardView extends StatefulWidget {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(20.0),
-          child: Column(
-            children: const [
-              //body
-            ],
+          child: Card(
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                children: [
+                  AutoCompleteWithRemoteDataSource(
+                    value: "",
+                    future: (search) async {
+                      var response = await Dio().get(
+                        "https://dummyjson.com/products/search?q=$search",
+                        options: Options(
+                          headers: {
+                            "Content-Type": "application/json",
+                          },
+                        ),
+                      );
+                      Map obj = response.data;
+                      return obj["products"]; //List
+                    },
+                    valueField: "id",
+                    displayField: "title",
+                    photoField: "thumbnail",
+                    onChanged: (value) {
+                      print("Your value is $value");
+                    },
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
       ),
