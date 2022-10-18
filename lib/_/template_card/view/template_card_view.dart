dd997f65495e39553266c2836b0dceaa7c0772f9
Deny <flutterlabz@gmail.com>
2022-10-18 11:28:12 +0700

    .

--- lib/_/template_card/view/template_card_view.dart
+++ lib/_/template_card/view/template_card_view.dart
@@ -1,4 +1,5 @@
 import 'package:blur/blur.dart';
+import 'package:fhe_template/_/__dummy/dummy_template.dart';
 import 'package:fhe_template/_/template_card/widget/rifqi_product_item.dart';
 import 'package:fhe_template/core.dart';
 import 'package:flutter/material.dart';
@@ -22,6 +23,136 @@ class TemplateCardView extends StatefulWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              ListView.builder(
+                itemCount: DummyTemplate().products.length,
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  var item = DummyTemplate().products[index];
+
+                  return Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            width: 90,
+                            height: 90,
+                            margin: const EdgeInsets.only(right: 10.0),
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: NetworkImage(
+                                  item["photo"],
+                                ),
+                                fit: BoxFit.cover,
+                              ),
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(16.0),
+                              ),
+                              color: Colors.blue[400],
+                            ),
+                            child: Stack(
+                              children: [
+                                Container(
+                                  padding: const EdgeInsets.all(6.0),
+                                  margin: const EdgeInsets.all(8.0),
+                                  decoration: BoxDecoration(
+                                    color: Colors.green[800],
+                                    borderRadius: const BorderRadius.all(
+                                      Radius.circular(
+                                        12.0,
+                                      ),
+                                    ),
+                                  ),
+                                  child: const Text(
+                                    "PROMO",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 8.0,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "${item["product_name"]}",
+                                  style: const TextStyle(
+                                    fontSize: 14.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 6.0,
+                                ),
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    const Text(
+                                      "8.1 km",
+                                      style: TextStyle(
+                                        fontSize: 10.0,
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      width: 4.0,
+                                    ),
+                                    const Icon(
+                                      Icons.circle,
+                                      size: 4.0,
+                                    ),
+                                    const SizedBox(
+                                      width: 4.0,
+                                    ),
+                                    Icon(
+                                      Icons.star,
+                                      color: Colors.orange[400],
+                                      size: 16.0,
+                                    ),
+                                    const Text(
+                                      "4.8",
+                                      style: TextStyle(
+                                        fontSize: 10.0,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(
+                                  height: 6.0,
+                                ),
+                                Text(
+                                  "${item["category"]}",
+                                  style: const TextStyle(
+                                    fontSize: 10.0,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 6.0,
+                                ),
+                                Text(
+                                  "€${item["price"]}",
+                                  style: const TextStyle(
+                                    fontSize: 14.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
               //#GROUP_TEMPLATE list_item
               Container(
                 height: 160.0,
