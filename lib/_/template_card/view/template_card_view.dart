4ab01afd08ce495d11dbe241e04636ee07aaf317
Deny <flutterlabz@gmail.com>
2022-10-22 10:28:22 +0700

    .

--- lib/_/template_card/view/template_card_view.dart
+++ lib/_/template_card/view/template_card_view.dart
@@ -21,6 +21,58 @@ class TemplateCardView extends StatefulWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              ElevatedButton.icon(
+                icon: const Icon(Icons.add),
+                label: const Text("Add"),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueGrey,
+                ),
+                onPressed: () async {
+                  bool confirm = false;
+                  await showDialog<void>(
+                    context: context,
+                    barrierDismissible: true,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: const Text('Confirm'),
+                        content: SingleChildScrollView(
+                          child: ListBody(
+                            children: const <Widget>[
+                              Text(
+                                  'Are you sure you want to delete this item?'),
+                            ],
+                          ),
+                        ),
+                        actions: <Widget>[
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.grey[600],
+                            ),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: const Text("No"),
+                          ),
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.blueGrey,
+                            ),
+                            onPressed: () {
+                              confirm = true;
+                              Navigator.pop(context);
+                            },
+                            child: const Text("Yes"),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+
+                  if (confirm) {
+                    print("Confirmed!");
+                  }
+                },
+              ),
               const SizedBox(
                 height: 20.0,
               ),
