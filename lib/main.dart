8be730e12be15b404133e39dc3a70fbdaebee31f
Deny <flutterlabz@gmail.com>
2022-10-23 11:46:22 +0700

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/foundation.dart';
 import 'dart:io';
-import 'package:example/_/template_card/view/template_card_view.dart';
 import 'package:example/core.dart';
 import 'package:flutter/material.dart';
 
@@ -42,29 +41,6 @@ class ShellRouteExampleApp extends StatelessWidget {
           //     : const PosLoginView(),
         );
       },
-    );
-    return MaterialApp(
-      title: 'Capek Ngoding',
-      navigatorKey: Get.navigatorKey,
-      debugShowCheckedModeBanner: false,
-      // theme: getDarkTheme(),
-      // theme: getDarkTheme(),
-      home: const TemplateCardView().useThemeEditor,
-      // home: const DebugView()
-      // home: FirebaseAuth.instance.currentUser != null
-      //     ? const PosMainNavigationView()
-      //     : const PosLoginView(),
-    );
-    return MaterialApp.router(
-      title: 'Capek Ngoding',
-      debugShowCheckedModeBanner: false,
-      theme: getDefaultTheme(),
-      routerConfig: getRouters(
-        context,
-        Get.navigatorKey,
-        Get.shellNavigatorKey,
-        "/hris_login",
-      ),
     );
   }
 }
