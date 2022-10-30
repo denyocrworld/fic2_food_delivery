c03111e0623a04f7503130dbba2b0d0a61f40e86
Deny <flutterlabz@gmail.com>
2022-10-30 15:27:23 +0700

    .

--- lib/module/form_ui/view/form_ui_view.dart
+++ lib/module/form_ui/view/form_ui_view.dart
@@ -1,5 +1,5 @@
+import 'package:example/core.dart';
 import 'package:flutter/material.dart';
-import '../controller/form_ui_controller.dart';
 
 class FormUiView extends StatefulWidget {
   const FormUiView({Key? key}) : super(key: key);
@@ -12,14 +12,40 @@ class FormUiView extends StatefulWidget {
         title: const Text("FormUi"),
         actions: const [],
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            children: const [
-              //body
-            ],
-          ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            //body
+            ExCard(
+              title: "Basic Form",
+              width: 240.0,
+              actions: const [],
+              bottomActions: [
+                ElevatedButton.icon(
+                  icon: const Icon(Icons.add),
+                  label: const Text("Add"),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blueGrey,
+                  ),
+                  onPressed: () {},
+                ),
+              ],
+              children: [
+                Container(
+                  height: 100.0,
+                  decoration: BoxDecoration(
+                    color: Colors.red[200],
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(
+                        16.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
--- lib/shared/widget/card/card.dart
+++ lib/shared/widget/card/card.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class ExCard extends StatelessWidget {
   final String title;
+  final double width;
   final IconData? icon;
   final List<Widget> children;
   final List<Widget> actions;
@@ -10,7 +11,8 @@ class ExCard extends StatelessWidget {
   const ExCard({
     Key? key,
     required this.title,
-    required this.children,
+    this.width = 240.0,
+    this.children = const [],
     this.padding,
     this.actions = const [],
     this.bottomActions = const [],
@@ -19,71 +21,74 @@ class ExCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 20.0,
-                horizontal: 24.0,
-              ),
-              child: Row(
-                children: [
-                  if (icon != null) ...[
-                    Icon(icon),
-                    const SizedBox(
-                      width: 6.0,
-                    ),
-                  ],
-                  Expanded(
-                    child: Text(
-                      title,
-                      style: const TextStyle(
-                        fontSize: 16.0,
+    return SizedBox(
+      width: width,
+      height: 300.0,
+      child: Card(
+        child: SizedBox(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 20.0,
+                  horizontal: 24.0,
+                ),
+                child: Row(
+                  children: [
+                    if (icon != null) ...[
+                      Icon(icon),
+                      const SizedBox(
+                        width: 6.0,
+                      ),
+                    ],
+                    Expanded(
+                      child: Text(
+                        title,
+                        style: const TextStyle(
+                          fontSize: 16.0,
+                        ),
                       ),
                     ),
-                  ),
-                  Row(
-                    children: List.generate(
-                      actions.length,
-                      (index) {
-                        return SizedBox(
-                          height: 30.0,
-                          child: FittedBox(
-                            child: actions[index],
-                          ),
-                        );
-                      },
+                    Row(
+                      children: List.generate(
+                        actions.length,
+                        (index) {
+                          return SizedBox(
+                            height: 30.0,
+                            child: FittedBox(
+                              child: actions[index],
+                            ),
+                          );
+                        },
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            const Divider(
-              height: 1.0,
-            ),
-            Container(
-              padding: padding,
-              child: Column(
-                children: children,
-              ),
-            ),
-            if (bottomActions.isNotEmpty) ...[
               const Divider(
                 height: 1.0,
               ),
               Container(
-                padding: const EdgeInsets.all(12.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: bottomActions,
+                padding: padding ?? const EdgeInsets.all(8.0),
+                child: Column(
+                  children: children,
                 ),
               ),
+              if (bottomActions.isNotEmpty) ...[
+                const Divider(
+                  height: 1.0,
+                ),
+                Container(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: bottomActions,
+                  ),
+                ),
+              ],
             ],
-          ],
+          ),
         ),
       ),
     );
