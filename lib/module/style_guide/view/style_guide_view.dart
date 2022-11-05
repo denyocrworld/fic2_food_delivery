f5742d8910e7e435dccff74a0ee4cdb265b04616
Deny <flutterlabz@gmail.com>
2022-11-05 10:05:41 +0700

    .

--- lib/module/style_guide/view/style_guide_view.dart
+++ lib/module/style_guide/view/style_guide_view.dart
@@ -1,6 +1,5 @@
+import 'package:example/core.dart';
 import 'package:flutter/material.dart';
-import 'package:example/state_util.dart';
-import '../controller/style_guide_controller.dart';
 
 class StyleGuideView extends StatefulWidget {
   const StyleGuideView({Key? key}) : super(key: key);
@@ -17,8 +16,204 @@ class StyleGuideView extends StatefulWidget {
         child: Padding(
           padding: const EdgeInsets.all(20.0),
           child: Column(
-            children: const [
+            children: [
               //body
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Column(
+                      children: [
+                        ExCard(
+                          title: "Base Color",
+                          children: [
+                            Row(
+                              children: [
+                                Container(
+                                  width: 60.0,
+                                  height: 60.0,
+                                  decoration: BoxDecoration(
+                                    color: successColor,
+                                    borderRadius: const BorderRadius.all(
+                                      Radius.circular(12.0),
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: 8.0,
+                                ),
+                                Container(
+                                  width: 60.0,
+                                  height: 60.0,
+                                  decoration: BoxDecoration(
+                                    color: warningColor,
+                                    borderRadius: const BorderRadius.all(
+                                      Radius.circular(12.0),
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: 8.0,
+                                ),
+                                Container(
+                                  width: 60.0,
+                                  height: 60.0,
+                                  decoration: BoxDecoration(
+                                    color: dangerColor,
+                                    borderRadius: const BorderRadius.all(
+                                      Radius.circular(12.0),
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: 8.0,
+                                ),
+                                Container(
+                                  width: 60.0,
+                                  height: 60.0,
+                                  decoration: BoxDecoration(
+                                    color: infoColor,
+                                    borderRadius: const BorderRadius.all(
+                                      Radius.circular(12.0),
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: 8.0,
+                                ),
+                                Container(
+                                  width: 60.0,
+                                  height: 60.0,
+                                  decoration: BoxDecoration(
+                                    color: primaryColor,
+                                    borderRadius: const BorderRadius.all(
+                                      Radius.circular(12.0),
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: 8.0,
+                                ),
+                                Container(
+                                  width: 60.0,
+                                  height: 60.0,
+                                  decoration: BoxDecoration(
+                                    color: disabledColor,
+                                    borderRadius: const BorderRadius.all(
+                                      Radius.circular(12.0),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 12.0,
+                        ),
+                        ExCard(
+                          title: "Scale",
+                          children: [
+                            Container(
+                              width: MediaQuery.of(context).size.width,
+                              height: 30,
+                              decoration: const BoxDecoration(
+                                color: Colors.orange,
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(12.0),
+                                ),
+                              ),
+                              child: const Center(
+                                child: Text(
+                                  "Extra Small",
+                                  textAlign: TextAlign.left,
+                                  style: TextStyle(
+                                    fontSize: 8.0,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 8.0,
+                            ),
+                            Container(
+                              width: MediaQuery.of(context).size.width,
+                              height: 40,
+                              decoration: const BoxDecoration(
+                                color: Colors.orange,
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(12.0),
+                                ),
+                              ),
+                              child: const Center(
+                                child: Text(
+                                  "Small",
+                                  textAlign: TextAlign.left,
+                                  style: TextStyle(
+                                    fontSize: 8.0,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 12.0,
+                  ),
+                  const Expanded(
+                    child: ExCard(
+                      title: "Heading",
+                      children: [
+                        Text(
+                          "Heading H1",
+                          style: TextStyle(
+                            fontSize: 30.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "Heading h2",
+                          style: TextStyle(
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "Heading h3",
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "Heading h4",
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "Heading h5",
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "Heading h6",
+                          style: TextStyle(
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
