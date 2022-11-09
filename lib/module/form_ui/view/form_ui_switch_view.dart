c0cfbe6ba994e31acd73fd2a7ab8b72534cd7573
Deny <flutterlabz@gmail.com>
2022-11-09 13:54:10 +0700

    .

--- lib/module/form_ui/view/form_ui_switch_view.dart
+++ lib/module/form_ui/view/form_ui_switch_view.dart
@@ -1,6 +1,8 @@
 import 'package:example/core.dart';
 import 'package:flutter/material.dart';
 
+GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
 class FormUISwitchView extends StatelessWidget {
   const FormUISwitchView({Key? key}) : super(key: key);
 
@@ -21,6 +23,49 @@ class FormUISwitchView extends StatelessWidget {
           value: false,
           groupValue: "gender",
           onChanged: (value) {},
+        ),
+        Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              FormField(
+                initialValue: false,
+                validator: (value) {
+                  if (value == null || value == false) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+                enabled: true,
+                builder: (FormFieldState<bool> field) {
+                  return InputDecorator(
+                    decoration: InputDecoration(
+                      labelText: 'Subscribe to mailing list.',
+                      errorText: field.errorText,
+                    ),
+                    child: SwitchListTile(
+                      title: const Text("Subscribe to our mailing list."),
+                      value: true,
+                      onChanged: (val) {
+                        field.didChange(val);
+                      },
+                    ),
+                  );
+                },
+              ),
+              ElevatedButton.icon(
+                icon: const Icon(Icons.save),
+                label: const Text("Save"),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueGrey,
+                ),
+                onPressed: () {
+                  var validation = _formKey.currentState!.validate();
+                  print(validation);
+                },
+              ),
+            ],
+          ),
         ),
       ],
     );
