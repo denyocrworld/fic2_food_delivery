1aa942a38d3b12d2bb7f31f24b3724149dd025c7
Deny <flutterlabz@gmail.com>
2022-11-13 06:06:35 +0700

    .

--- lib/main.dart
+++ lib/main.dart
@@ -13,33 +13,6 @@ void main() async {
     await FirebaseNotification.initialize();
   }
 
-  return runApp(MaterialApp(
-    title: 'Capek Ngoding',
-<<<<<<< HEAD
-    navigatorKey: Get.navigatorKey,
-    debugShowCheckedModeBanner: false,
-    theme: getDarkTheme(),
-    home: const TestxxxView(),
-=======
-    debugShowCheckedModeBanner: false,
-    theme: ThemeData.dark().copyWith(
-      // scaffoldBackgroundColor: Colors.brown[200],
-      // appBarTheme: const AppBarTheme(
-      //   backgroundColor: Colors.purple,
-      // ),
-      cardTheme: CardTheme(
-        elevation: 10,
-        // color: Colors.green[100],
-        shape: ContinuousRectangleBorder(
-          borderRadius: BorderRadius.circular(64.0),
-        ),
-      ),
-    ),
-    home: const ProfileUiView(),
-    // home: const FormUiView(),
->>>>>>> 015bfb1b552214b52fa2681be302ab39fdaa5575
-  ));
-
   runApp(MaterialApp.router(
     title: 'Capek Ngoding',
     debugShowCheckedModeBanner: false,
