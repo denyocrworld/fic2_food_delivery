e85c6c9c42447a338d171af520157444c2f6a311
Deny <flutterlabz@gmail.com>
2022-11-15 12:22:08 +0700

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,16 @@
-import 'package:flutter/foundation.dart';
-import 'dart:io';
 import 'package:example/core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   await initialize();
-  if (!kIsWeb && !Platform.isWindows) await UserService.initialize();
-  if (!kIsWeb && !Platform.isWindows) {
-    await FirebaseNotification.initialize();
-  }
 
-  // return runApp(MaterialApp(
-  //   title: 'Capek Ngoding',
-  //   navigatorKey: Get.navigatorKey,
-  //   debugShowCheckedModeBanner: false,
-  //   theme: ThemeData.dark(),
-  //   home: const TrView(),
-  // ));
+  return runApp(MaterialApp(
+    title: 'Capek Ngoding',
+    navigatorKey: Get.navigatorKey,
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData.dark(),
+    home: const TrView(),
+  ));
 
   runApp(MaterialApp.router(
     title: 'Capek Ngoding',
