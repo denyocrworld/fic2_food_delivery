63c7bc2f66b6b43dcf52ce1c9c505c6aef1811ac
msyamsul725 <msyamsul7279@gmail.com>
2022-11-15 07:36:12 +0700

    .

--- lib/module/main_navigation/view/main_navigation_view.dart
+++ lib/module/main_navigation/view/main_navigation_view.dart
@@ -202,7 +202,7 @@ class MainNavigationViewState extends State<MainNavigationView> {
       route: '/pos_another_example',
       items: [],
     ),
-<<<<<<< HEAD
+
     NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Profile Ui Example',
@@ -221,256 +221,253 @@ class MainNavigationViewState extends State<MainNavigationView> {
       route: '/profile_ui',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Tr State Management',
       route: '/tr_state_management',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Tr Form',
       route: '/tr_form',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trsm Counter',
       route: '/trsm_counter',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trsm Loading',
       route: '/trsm_loading',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trsm Visibility',
       route: '/trsm_visibility',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trsm Navigation',
       route: '/trsm_navigation',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trsm Enabled And Disabled',
       route: '/trsm_enabled_and_disabled',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trsm Cart',
       route: '/trsm_cart',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trsm Add And Delete From List',
       route: '/trsm_add_and_delete_from_list',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trsm Loading For Http Request',
       route: '/trsm_loading_for_http_request',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trsm Animation',
       route: '/trsm_animation',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trsm Digital Clock',
       route: '/trsm_digital_clock',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trsm Crud',
       route: '/trsm_crud',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Tr',
       route: '/tr',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trfm Login',
       route: '/trfm_login',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trfm Login Form',
       route: '/trfm_login_form',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trfm Signup Form',
       route: '/trfm_signup_form',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trfm Forgot Password Form',
       route: '/trfm_forgot_password_form',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trfm Reset Password Form',
       route: '/trfm_reset_password_form',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trfm Edit Profile Form',
       route: '/trfm_edit_profile_form',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trfm Product Form',
       route: '/trfm_product_form',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trfm Checkout Form',
       route: '/trfm_checkout_form',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trfm Review Form',
       route: '/trfm_review_form',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trfm Filter Date Dialog Form',
       route: '/trfm_filter_date_dialog_form',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trfm Filter Category And Date Bottomsheet Form',
       route: '/trfm_filter_category_and_date_bottomsheet_form',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trsm Filter List',
       route: '/trsm_filter_list',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trsm Horizontal Category List',
       route: '/trsm_horizontal_category_list',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trsm Vertical Category List',
       route: '/trsm_vertical_category_list',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trsm Category In Wrap',
       route: '/trsm_category_in_wrap',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trfm Edit Store Form',
       route: '/trfm_edit_store_form',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trsm Slide Animation',
       route: '/trsm_slide_animation',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trsm Fade In Animation',
       route: '/trsm_fade_in_animation',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trsm Fade Out Animation',
       route: '/trsm_fade_out_animation',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trsm Scale In Animation',
       route: '/trsm_scale_in_animation',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trsm Scale Out Animation',
       route: '/trsm_scale_out_animation',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trsm Animation By Mouse Event',
       route: '/trsm_animation_by_mouse_event',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trsm Fade Animation',
       route: '/trsm_fade_animation',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trsm Scale Animation',
       route: '/trsm_scale_animation',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trsm Rotate Animation',
       route: '/trsm_rotate_animation',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trsm Scale Animation By Slide Value',
       route: '/trsm_scale_animation_by_slide_value',
       items: [],
     ),
-          NavigationItem(
+    NavigationItem(
       icon: const Icon(MdiIcons.web),
       label: 'Trsm Chat List',
       route: '/trsm_chat_list',
       items: [],
     ),
-          //@NAVIGATION_GENERATOR
-=======
     //@NAVIGATION_GENERATOR
->>>>>>> f5235c110c217889bac21d567a19f51d9c5b1b25
   ];
 
   @override
