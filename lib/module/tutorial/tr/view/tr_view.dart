c409cf2606ba3bf0c6044e7566ae93dd73d7e623
Deny <flutterlabz@gmail.com>
2022-11-16 16:34:52 +0700

    .

--- lib/module/tutorial/tr/view/tr_view.dart
+++ lib/module/tutorial/tr/view/tr_view.dart
@@ -1,3 +1,4 @@
+import 'package:example/shared/widget/form/__form_example_non_reuseable.dart';
 import 'package:flutter/material.dart';
 import 'package:example/core.dart';
 
@@ -115,20 +116,47 @@ class TrView extends StatefulWidget {
                 );
               }),
               header("Form Exercise"),
-              ElevatedButton.icon(
-                icon: const Icon(Icons.app_registration_rounded),
-                label: const Text("Form Example"),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blueGrey,
-                ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const FormExampleView(),
+              Row(
+                children: [
+                  Expanded(
+                    child: ElevatedButton.icon(
+                      icon: const Icon(Icons.app_registration_rounded),
+                      label: const Text("Form"),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blueGrey,
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const FormExampleView(),
+                          ),
+                        );
+                      },
                     ),
-                  );
-                },
+                  ),
+                  const SizedBox(
+                    width: 12.0,
+                  ),
+                  Expanded(
+                    child: ElevatedButton.icon(
+                      icon: const Icon(Icons.app_registration_rounded),
+                      label: const Text("Form (Reuseable)"),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blueGrey,
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                const FormExampleNonReuseableView(),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
               ),
               const SizedBox(
                 height: 20.0,
