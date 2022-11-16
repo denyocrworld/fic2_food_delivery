36e71ee4e7e31a8e99b54b86615f7ce5471b940c
Deny <flutterlabz@gmail.com>
2022-11-16 10:01:06 +0700

    .

--- lib/shared/widget/form/__form_example.dart
+++ lib/shared/widget/form/__form_example.dart
@@ -11,164 +11,250 @@ class FormExampleView extends StatefulWidget {
 
 class _FormExampleViewState extends State<FormExampleView> {
   GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  bool lightMode = false;
+
+  ThemeData get lightTheme {
+    return ThemeData.light().copyWith(
+      primaryColor: Colors.blueGrey,
+      appBarTheme: const AppBarTheme(
+        backgroundColor: Colors.blueGrey,
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.blueGrey[900],
+        ),
+      ),
+    );
+  }
+
+  ThemeData get darkTheme {
+    return ThemeData.dark().copyWith(
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.orange[900],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Form Test"),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: ElevatedButton.icon(
-              icon: const Icon(Icons.login),
-              label: const Text("Save"),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blueGrey,
-              ),
+    return Theme(
+      data: lightMode ? lightTheme : darkTheme,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Form Test"),
+          actions: [
+            IconButton(
               onPressed: () {
-                bool isValid = formKey.currentState!.validate();
+                lightMode = !lightMode;
+                setState(() {});
               },
+              icon: const Icon(
+                Icons.light_mode,
+                size: 24.0,
+              ),
             ),
-          ),
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(20.0),
-          child: Form(
-            key: formKey,
-            child: Column(
-              children: [
-                QTextField(
-                  label: "Email",
-                  hint: "Your email",
-                  validator: Validator.required,
-                  value: "demo@gmail.com",
-                  onChanged: (value) {},
-                ),
-                QTextField(
-                  label: "Password",
-                  hint: "Your password",
-                  obscure: true,
-                  validator: Validator.required,
-                  value: "123456",
-                  onChanged: (value) {},
-                ),
-                QNumberField(
-                  label: "Age",
-                  hint: "Your age's",
-                  validator: Validator.required,
-                  value: "24",
-                  onChanged: (value) {},
-                ),
-                QMemoField(
-                  label: "Address",
-                  hint: "Your addres",
-                  validator: Validator.required,
-                  value: "Kamboja street 16, Bogor, West Java, Indonesia",
-                  onChanged: (value) {},
-                ),
-                const QRadioField(
-                  label: "Member",
-                  validator: Validator.atLeastOneitem,
-                  items: [
-                    {
-                      "label": "Private",
-                      "value": "Private",
-                    },
-                    {
-                      "label": "Premium",
-                      "value": "Premium",
-                    }
-                  ],
-                ),
-                const QCheckField(
-                  id: "club",
-                  label: "Club",
-                  validator: Validator.atLeastOneitem,
-                  items: [
-                    {
-                      "label": "Persib",
-                      "value": "Persib",
-                      "checked": false,
-                    },
-                    {
-                      "label": "Persikabo",
-                      "value": "Persikabo",
-                      "checked": true,
-                    }
-                  ],
-                ),
-                const QSwitch(
-                  id: "member",
-                  label: "Member",
-                  validator: Validator.atLeastOneitem,
-                  items: [
-                    {
-                      "label": "Private",
-                      "value": "Private",
-                    },
-                    {
-                      "label": "Premium",
-                      "value": "Premium",
-                    }
-                  ],
-                ),
-                QDropdownField(
-                  label: "Membership",
-                  validator: Validator.required,
-                  items: const [
-                    {
-                      "label": "Regular",
-                      "value": "1",
-                    },
-                    {
-                      "label": "Premium",
-                      "value": "2",
-                    }
-                  ],
-                  onChanged: (value, label) {
-                    print("New Label: $label #");
-                    print("New Value: $value #");
-                  },
-                ),
-                QDropdownField(
-                  label: "Membership",
-                  validator: Validator.required,
-                  items: const [
-                    {
-                      "label": "Regular",
-                      "value": "1",
-                    },
-                    {
-                      "label": "Premium",
-                      "value": "2",
-                    }
-                  ],
-                  value: "Regular",
-                  onChanged: (value, label) {
-                    print("New Label: $label #");
-                    print("New Value: $value #");
-                  },
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                ElevatedButton.icon(
-                  icon: const Icon(Icons.login),
-                  label: const Text("Save"),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blueGrey,
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: ElevatedButton.icon(
+                icon: const Icon(Icons.login),
+                label: const Text("Save"),
+                style: ElevatedButton.styleFrom(
+                    // backgroundColor: Colors.blueGrey,
+                    ),
+                onPressed: () {
+                  bool isValid = formKey.currentState!.validate();
+                },
+              ),
+            ),
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(20.0),
+            child: Form(
+              key: formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Basic Form",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                  onPressed: () {
-                    bool isValid = formKey.currentState!.validate();
-                  },
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-              ],
+                  QTextField(
+                    label: "Email",
+                    hint: "Your email",
+                    validator: Validator.required,
+                    value: "demo@gmail.com",
+                    onChanged: (value) {},
+                  ),
+                  QTextField(
+                    label: "Password",
+                    hint: "Your password",
+                    obscure: true,
+                    validator: Validator.required,
+                    value: "123456",
+                    onChanged: (value) {},
+                  ),
+                  QNumberField(
+                    label: "Age",
+                    hint: "Your age's",
+                    validator: Validator.required,
+                    value: "24",
+                    onChanged: (value) {},
+                  ),
+                  QMemoField(
+                    label: "Address",
+                    hint: "Your addres",
+                    validator: Validator.required,
+                    value: "Kamboja street 16, Bogor, West Java, Indonesia",
+                    onChanged: (value) {},
+                  ),
+                  const QRadioField(
+                    label: "Member",
+                    validator: Validator.atLeastOneitem,
+                    items: [
+                      {
+                        "label": "Private",
+                        "value": "Private",
+                      },
+                      {
+                        "label": "Premium",
+                        "value": "Premium",
+                      }
+                    ],
+                  ),
+                  const QCheckField(
+                    id: "club",
+                    label: "Club",
+                    validator: Validator.atLeastOneitem,
+                    items: [
+                      {
+                        "label": "Persib",
+                        "value": "Persib",
+                        "checked": false,
+                      },
+                      {
+                        "label": "Persikabo",
+                        "value": "Persikabo",
+                        "checked": true,
+                      }
+                    ],
+                  ),
+                  const QSwitch(
+                    id: "member",
+                    label: "Member",
+                    validator: Validator.atLeastOneitem,
+                    items: [
+                      {
+                        "label": "Private",
+                        "value": "Private",
+                      },
+                      {
+                        "label": "Premium",
+                        "value": "Premium",
+                      }
+                    ],
+                  ),
+                  QDropdownField(
+                    label: "Membership",
+                    validator: Validator.required,
+                    items: const [
+                      {
+                        "label": "Regular",
+                        "value": "1",
+                      },
+                      {
+                        "label": "Premium",
+                        "value": "2",
+                      }
+                    ],
+                    onChanged: (value, label) {
+                      print("New Label: $label #");
+                      print("New Value: $value #");
+                    },
+                  ),
+                  QDropdownField(
+                    label: "Membership",
+                    validator: Validator.required,
+                    items: const [
+                      {
+                        "label": "Regular",
+                        "value": "1",
+                      },
+                      {
+                        "label": "Premium",
+                        "value": "2",
+                      }
+                    ],
+                    value: "Regular",
+                    onChanged: (value, label) {
+                      print("New Label: $label #");
+                      print("New Value: $value #");
+                    },
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  const Text(
+                    "Advanced Form",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const QAutoComplete(
+                    id: "member",
+                    label: "Member",
+                    validator: Validator.required,
+                    items: [
+                      {
+                        "label": "Jackie Roo",
+                        "info": "Hacker",
+                      },
+                      {
+                        "label": "Dan Milton",
+                        "info": "UI/UX Designer",
+                      },
+                      {
+                        "label": "Ryper Roo",
+                        "info": "Android Developer",
+                      }
+                    ],
+                  ),
+                  const QAutoComplete(
+                    id: "member",
+                    label: "Member",
+                    validator: Validator.required,
+                    items: [
+                      {
+                        "photo": "https://i.ibb.co/PGv8ZzG/me.jpg",
+                        "label": "John Doe",
+                        "info": "Programmer"
+                      },
+                      {
+                        "photo":
+                            "https://i.ibb.co/5v5RLKr/photo-1528813860492-bb99459ec095-crop-entropy-cs-tinysrgb-fit-max-fm-jpg-ixid-Mnwy-ODA4-ODh8-MHwxf-H.jpg",
+                        "label": "Renata",
+                        "info": "System Analyst"
+                      },
+                      {
+                        "photo":
+                            "https://i.ibb.co/mR8PJCz/photo-1576570734316-e9d0317d7384-crop-entropy-cs-tinysrgb-fit-max-fm-jpg-ixid-Mnwy-ODA4-ODh8-MHwxf-H.jpg",
+                        "label": "Kayla",
+                        "info": "Test Engineer"
+                      }
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 300.0,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/shared/widget/form/autocomplete.dart
+++ lib/shared/widget/form/autocomplete.dart
@@ -32,134 +32,135 @@ class _QAutoCompleteState extends State<QAutoComplete> {
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constraints) {
-      return Autocomplete<Map>(
-        fieldViewBuilder:
-            (context, textEditingController, focusNode, onFieldSubmitted) {
-          return TextFormField(
-            controller: textEditingController,
-            focusNode: focusNode,
-            onFieldSubmitted: (text) => onFieldSubmitted(),
-            validator: widget.validator,
-            decoration: const InputDecoration(
-              labelText: 'Search',
-              labelStyle: TextStyle(
-                color: Colors.blueGrey,
+      return FormField(
+          initialValue: false,
+          // validator: (value) => widget.validator!(items),
+          enabled: true,
+          builder: (field) {
+            return Autocomplete<Map>(
+              fieldViewBuilder: (context, textEditingController, focusNode,
+                  onFieldSubmitted) {
+                return TextFormField(
+                  controller: textEditingController,
+                  focusNode: focusNode,
+                  onFieldSubmitted: (text) => onFieldSubmitted(),
+                  validator: widget.validator,
+                  decoration: InputDecoration(
+                    labelText: widget.label,
+                    errorText: field.errorText,
+                    // border: InputBorder.none,
+                    suffixIcon: const Icon(Icons.search),
+                  ),
+                );
+              },
+              initialValue: TextEditingValue(
+                text: items.first["label"],
               ),
-              suffixIcon: Icon(
-                Icons.search,
-              ),
-              enabledBorder: UnderlineInputBorder(
-                borderSide: BorderSide(
-                  color: Colors.blueGrey,
-                ),
-              ),
-              helperText: "Let's start with some characters",
-            ),
-          );
-        },
-        initialValue: TextEditingValue(
-          text: items.first["label"],
-        ),
-        onSelected: (Map value) {
-          //selected value
-        },
-        optionsBuilder: (TextEditingValue textEditingValue) {
-          if (textEditingValue.text == '') {
-            return const Iterable<Map>.empty();
-          }
-          return items.where((Map option) {
-            return option["label"]
-                .toString()
-                .toLowerCase()
-                .contains(textEditingValue.text.toLowerCase());
-          });
-        },
-        displayStringForOption: (option) {
-          return option["label"];
-        },
-        optionsViewBuilder: (context, onSelected, options) => Align(
-          alignment: Alignment.topLeft,
-          child: Material(
-            color: Colors.transparent,
-            child: ClipRRect(
-              borderRadius: const BorderRadius.vertical(
-                bottom: Radius.circular(4.0),
-              ),
-              child: Container(
-                width: constraints.biggest.width,
-                // width: MediaQuery.of(context).size.width,
-                margin: const EdgeInsets.only(top: 10.0),
-                child: Wrap(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        // color: Colors.grey[100],
-                        color: Theme.of(context).scaffoldBackgroundColor,
-                        borderRadius: const BorderRadius.all(
-                          Radius.circular(12.0),
-                        ),
-                        border: Border.all(
-                          width: 1.0,
-                          color: Theme.of(context)
-                              .scaffoldBackgroundColor
-                              .withOpacity(0.4),
-                        ),
-                      ),
-                      child: ListView.builder(
-                        padding: EdgeInsets.zero,
-                        itemCount: options.length,
-                        shrinkWrap: true,
-                        itemBuilder: (BuildContext context, int index) {
-                          bool selected =
-                              AutocompleteHighlightedOption.of(context) ==
-                                  index;
-                          Map option = options.elementAt(index);
-
-                          return InkWell(
-                            onTap: () => onSelected(option),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                color: selected
-                                    ? Theme.of(context).focusColor
-                                    : null,
-                                borderRadius: BorderRadius.only(
-                                  topLeft: Radius.circular(
-                                    index == 0 ? 12 : 0,
-                                  ),
-                                  topRight: Radius.circular(
-                                    index == 0 ? 12 : 0,
-                                  ),
-                                  bottomLeft: Radius.circular(
-                                    index == options.length - 1 ? 12 : 0.0,
-                                  ),
-                                  bottomRight: Radius.circular(
-                                    index == options.length - 1 ? 12 : 0.0,
-                                  ),
-                                ),
+              onSelected: (Map value) {
+                //selected value
+              },
+              optionsBuilder: (TextEditingValue textEditingValue) {
+                if (textEditingValue.text == '') {
+                  return const Iterable<Map>.empty();
+                }
+                return items.where((Map option) {
+                  return option["label"]
+                      .toString()
+                      .toLowerCase()
+                      .contains(textEditingValue.text.toLowerCase());
+                });
+              },
+              displayStringForOption: (option) {
+                return option["label"];
+              },
+              optionsViewBuilder: (context, onSelected, options) => Align(
+                alignment: Alignment.topLeft,
+                child: Material(
+                  color: Colors.transparent,
+                  child: ClipRRect(
+                    borderRadius: const BorderRadius.vertical(
+                      bottom: Radius.circular(4.0),
+                    ),
+                    child: Container(
+                      width: constraints.biggest.width,
+                      // width: MediaQuery.of(context).size.width,
+                      margin: const EdgeInsets.only(top: 10.0),
+                      child: Wrap(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              // color: Colors.grey[100],
+                              color: Theme.of(context).scaffoldBackgroundColor,
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(12.0),
                               ),
-                              child: ListTile(
-                                leading: option["photo"] == null
-                                    ? null
-                                    : CircleAvatar(
-                                        backgroundImage: NetworkImage(
-                                          option["photo"],
-                                        ),
-                                      ),
-                                title: Text("${option["label"]}"),
-                                subtitle: Text("${option["info"]}"),
+                              border: Border.all(
+                                width: 1.0,
+                                color: Theme.of(context)
+                                    .scaffoldBackgroundColor
+                                    .withOpacity(0.4),
                               ),
                             ),
-                          );
-                        },
+                            child: ListView.builder(
+                              padding: EdgeInsets.zero,
+                              itemCount: options.length,
+                              shrinkWrap: true,
+                              itemBuilder: (BuildContext context, int index) {
+                                bool selected =
+                                    AutocompleteHighlightedOption.of(context) ==
+                                        index;
+                                Map option = options.elementAt(index);
+
+                                return InkWell(
+                                  onTap: () => onSelected(option),
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      color: selected
+                                          ? Theme.of(context).focusColor
+                                          : null,
+                                      borderRadius: BorderRadius.only(
+                                        topLeft: Radius.circular(
+                                          index == 0 ? 12 : 0,
+                                        ),
+                                        topRight: Radius.circular(
+                                          index == 0 ? 12 : 0,
+                                        ),
+                                        bottomLeft: Radius.circular(
+                                          index == options.length - 1
+                                              ? 12
+                                              : 0.0,
+                                        ),
+                                        bottomRight: Radius.circular(
+                                          index == options.length - 1
+                                              ? 12
+                                              : 0.0,
+                                        ),
+                                      ),
+                                    ),
+                                    child: ListTile(
+                                      leading: option["photo"] == null
+                                          ? null
+                                          : CircleAvatar(
+                                              backgroundImage: NetworkImage(
+                                                option["photo"],
+                                              ),
+                                            ),
+                                      title: Text("${option["label"]}"),
+                                      subtitle: Text("${option["info"]}"),
+                                    ),
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                  ],
+                  ),
                 ),
               ),
-            ),
-          ),
-        ),
-      );
+            );
+          });
     });
   }
 }
