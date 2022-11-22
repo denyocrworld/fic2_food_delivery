9f40ffd2952839cb42c4b9d4bf5194f399d35893
magerngulik <magerngulik@gmail.com>
2022-11-22 08:43:47 +0700

    ,

--- lib/module/dialog_ui/view/dialog_ui_view.dart
+++ lib/module/dialog_ui/view/dialog_ui_view.dart
@@ -1,6 +1,5 @@
+import 'package:example/core.dart';
 import 'package:flutter/material.dart';
-import 'package:example/state_util.dart';
-import '../controller/dialog_ui_controller.dart';
 
 class DialogUiView extends StatefulWidget {
   const DialogUiView({Key? key}) : super(key: key);
@@ -17,8 +16,22 @@ class DialogUiView extends StatefulWidget {
         child: Padding(
           padding: const EdgeInsets.all(20.0),
           child: Column(
-            children: const [
-              //body
+            children: [
+              ExCard(
+                title: "Snackbar Dialog",
+                children: [
+                  ElevatedButton.icon(
+                    icon: const Icon(Icons.add),
+                    label: const Text("Add"),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blueGrey,
+                    ),
+                    onPressed: () {
+                      snackbarInfo("data1");
+                    },
+                  ),
+                ],
+              )
             ],
           ),
         ),
