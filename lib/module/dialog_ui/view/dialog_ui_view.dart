b5b206a3e8f6f394a9e0d9b7160786169dd9ded6
magerngulik <magerngulik@gmail.com>
2022-11-23 19:49:44 +0700

    menambahkan alert icon basic

--- lib/module/dialog_ui/view/dialog_ui_view.dart
+++ lib/module/dialog_ui/view/dialog_ui_view.dart
@@ -17,19 +17,6 @@ class DialogUiView extends StatefulWidget {
           padding: const EdgeInsets.all(20.0),
           child: Column(
             children: [
-              ExCard(title: "Snackbar Dialog", children: [
-                ElevatedButton.icon(
-                  icon: const Icon(Icons.add),
-                  label: const Text("Add"),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blueGrey,
-                  ),
-                  onPressed: () {
-                    snackbarInfo(message: "data1", duration: 1);
-                  },
-                ),
-              ]),
-
               //body
               // var primaryColor = Colors.blueGrey[800]!;
               // var secondaryColor = const Color(0xFF2A2D3E);
@@ -214,6 +201,193 @@ class DialogUiView extends StatefulWidget {
                               color: warningColor,
                               onPressed: () {
                                 snackbarSoftWarning(
+                                    message:
+                                        "warning - A sample alert -- check it out!",
+                                    duration: 1);
+                              }),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              //icons alert
+              ExCard(
+                color: Colors.white,
+                width: MediaQuery.of(context).size.width * 0.8,
+                children: [
+                  Column(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.symmetric(vertical: 4.0),
+                        width: MediaQuery.of(context).size.width,
+                        child: const Text(
+                          "Icon Alert Soft",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24.0,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          ExButton(
+                              radius: 15,
+                              label: "primary",
+                              width: 100,
+                              color: primaryColor,
+                              onPressed: () {
+                                snackbarIconSoftPrimary(
+                                    message:
+                                        "primary - A sample alert -- check it out!",
+                                    duration: 1);
+                              }),
+                          ExButton(
+                              radius: 15,
+                              label: "secondary",
+                              width: 100,
+                              color: secondaryColor,
+                              onPressed: () {
+                                snackbarIconSoftSecondary(
+                                    message:
+                                        "secondary - A sample alert -- check it out!",
+                                    duration: 1);
+                              }),
+                          ExButton(
+                              radius: 15,
+                              label: "danger",
+                              width: 100,
+                              color: dangerColor,
+                              onPressed: () {
+                                snackbarIconSoftDanger(
+                                    message:
+                                        "danger - A sample alert -- check it out!",
+                                    duration: 1);
+                              }),
+                          ExButton(
+                              radius: 15,
+                              label: "success",
+                              width: 100,
+                              color: successColor,
+                              onPressed: () {
+                                snackbarIconSoftSuccess(
+                                    message:
+                                        "success - A sample alert -- check it out!",
+                                    duration: 1);
+                              }),
+                          ExButton(
+                              radius: 15,
+                              label: "info",
+                              width: 100,
+                              color: infoColor,
+                              onPressed: () {
+                                snackbarIconSoftInfo(
+                                    message:
+                                        "info - A sample alert -- check it out!",
+                                    duration: 1);
+                              }),
+                          ExButton(
+                              radius: 15,
+                              label: "warning",
+                              width: 100,
+                              color: warningColor,
+                              onPressed: () {
+                                snackbarIconSoftWarning(
+                                    message:
+                                        "warning - A sample alert -- check it out!",
+                                    duration: 1);
+                              }),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              ExCard(
+                color: Colors.white,
+                width: MediaQuery.of(context).size.width * 0.8,
+                children: [
+                  Column(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.symmetric(vertical: 4.0),
+                        width: MediaQuery.of(context).size.width,
+                        child: const Text(
+                          "Icon Alert Basic",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24.0,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          ExButton(
+                              radius: 15,
+                              label: "primary",
+                              width: 100,
+                              color: primaryColor,
+                              onPressed: () {
+                                snackbarIconPrimary(
+                                    message:
+                                        "primary - A sample alert -- check it out!",
+                                    duration: 1);
+                              }),
+                          ExButton(
+                              radius: 15,
+                              label: "secondary",
+                              width: 100,
+                              color: secondaryColor,
+                              onPressed: () {
+                                snackbarIconSecondary(
+                                    message:
+                                        "secondary - A sample alert -- check it out!",
+                                    duration: 1);
+                              }),
+                          ExButton(
+                              radius: 15,
+                              label: "danger",
+                              width: 100,
+                              color: dangerColor,
+                              onPressed: () {
+                                snackbarIconDanger(
+                                    message:
+                                        "danger - A sample alert -- check it out!",
+                                    duration: 1);
+                              }),
+                          ExButton(
+                              radius: 15,
+                              label: "success",
+                              width: 100,
+                              color: successColor,
+                              onPressed: () {
+                                snackbarIconSuccess(
+                                    message:
+                                        "success - A sample alert -- check it out!",
+                                    duration: 1);
+                              }),
+                          ExButton(
+                              radius: 15,
+                              label: "info",
+                              width: 100,
+                              color: infoColor,
+                              onPressed: () {
+                                snackbarIconInfo(
+                                    message:
+                                        "info - A sample alert -- check it out!",
+                                    duration: 1);
+                              }),
+                          ExButton(
+                              radius: 15,
+                              label: "warning",
+                              width: 100,
+                              color: warningColor,
+                              onPressed: () {
+                                snackbarIconWarning(
                                     message:
                                         "warning - A sample alert -- check it out!",
                                     duration: 1);
