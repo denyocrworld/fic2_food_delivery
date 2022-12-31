73e99d1b7e38fad225729eab227546e349495537
flutterlabz <flutterlabz@gmail.com>
2022-12-31 14:42:07 +0700

    .

--- lib/module/capekngoding/cg_main/widget/side_menu.dart
+++ lib/module/capekngoding/cg_main/widget/side_menu.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: camel_case_types, prefer_typing_uninitialized_variables
 
+import 'package:example/core.dart';
 import 'package:flutter/material.dart';
 
 class SideMenu extends StatelessWidget {
@@ -31,48 +32,56 @@ class SideMenu extends StatelessWidget {
               (index) {
                 var item = menuList[index];
 
-                return Container(
-                  width: size,
-                  height: size,
-                  margin: const EdgeInsets.all(4.0),
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    padding: const EdgeInsets.all(6.0),
-                    decoration: const BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(12.0),
-                      ),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Color(0x19000000),
-                          blurRadius: 12,
-                          offset: Offset(0, 4),
-                        ),
-                      ],
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        IconButton(
-                          icon: Icon(item["icon"]),
-                          color: item["color"],
-                          iconSize: 32.0,
-                          onPressed: () {},
-                        ),
-                        Expanded(
-                          child: Text(
-                            '${item["label"]}',
-                            maxLines: 2,
-                            overflow: TextOverflow.visible,
-                            textAlign: TextAlign.center,
-                            style: const TextStyle(
-                              fontSize: 10.0,
-                            ),
+                return InkWell(
+                  onTap: () {
+                    print("updateView to ${item["page"]}");
+                    CgMainController.instance.updateView(item["page"]);
+                  },
+                  child: AbsorbPointer(
+                    child: Container(
+                      width: size,
+                      height: size,
+                      margin: const EdgeInsets.all(4.0),
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        padding: const EdgeInsets.all(6.0),
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(12.0),
                           ),
-                        )
-                      ],
+                          boxShadow: [
+                            BoxShadow(
+                              color: Color(0x19000000),
+                              blurRadius: 12,
+                              offset: Offset(0, 4),
+                            ),
+                          ],
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            IconButton(
+                              icon: Icon(item["icon"]),
+                              color: item["color"],
+                              iconSize: 32.0,
+                              onPressed: () {},
+                            ),
+                            Expanded(
+                              child: Text(
+                                '${item["label"]}',
+                                maxLines: 2,
+                                overflow: TextOverflow.visible,
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(
+                                  fontSize: 10.0,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
                     ),
                   ),
                 );
