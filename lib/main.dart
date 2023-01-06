4c60c379f99a238bc9dd78c20d912863191f1a3f
flutterlabz <flutterlabz@gmail.com>
2023-01-06 23:07:41 +0700

    .

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
 
-/*
-
-*/
-
 void main() async {
   await initialize();
   if (!kIsWeb) {
