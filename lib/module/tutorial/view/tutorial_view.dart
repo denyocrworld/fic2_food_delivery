183cff09b792a8274f3ade9b4e4c4ade46ca2194
flutterlabz <flutterlabz@gmail.com>
2023-01-12 17:19:03 +0700

    .

--- lib/module/tutorial/view/tutorial_view.dart
+++ lib/module/tutorial/view/tutorial_view.dart
@@ -26,33 +26,6 @@ class TutorialView extends StatefulWidget {
         title: const Text("Dashboard"),
         actions: const [],
       ),
-      body: Container(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                itemCount: 10,
-                itemBuilder: (context, index) {
-                  var item = {};
-                  return Card(
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        backgroundColor: Colors.grey[200],
-                        backgroundImage: const NetworkImage(
-                          "https://i.ibb.co/QrTHd59/woman.jpg",
-                        ),
-                      ),
-                      title: const Text("Jessica Doe"),
-                      subtitle: const Text("Programmer"),
-                    ),
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
     );
 
     int counter = 2;
