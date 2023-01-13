34f75f2acedf2b8bf237a393d79c6106a43fe0a2
flutterlabz <flutterlabz@gmail.com>
2023-01-13 09:47:30 +0700

    .

--- lib/_/scaffold/scaffold.dart
+++ lib/_/scaffold/scaffold.dart
@@ -6,15 +6,36 @@ class TemplateScaffold extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //#GROUP_TEMPLATE scaffold
-    //#TEMPLATE scaffold
+
+    if (1 != 2) {
+      //#TEMPLATE scaffold
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text("Dashboard"),
+          actions: const [],
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              children: const [],
+            ),
+          ),
+        ),
+      );
+      //#END
+    }
+
+    //#TEMPLATE scaffoldc
     return Scaffold(
       appBar: AppBar(
         title: const Text("Dashboard"),
         actions: const [],
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(10.0),
+      body: Container(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: const [],
         ),
       ),
     );
