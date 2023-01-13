fc4f05eeefd197cf10ab6c5e6f6738788f368f64
flutterlabz <flutterlabz@gmail.com>
2023-01-13 16:45:47 +0700

    .

--- lib/module/capekngoding/cg_chart/view/cg_chart_view.dart
+++ lib/module/capekngoding/cg_chart/view/cg_chart_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:hyper_ui/core.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 import '../controller/cg_chart_controller.dart';
 
+//#GROUP_TEMPLATE chart
 class CgChartView extends StatefulWidget {
   const CgChartView({Key? key}) : super(key: key);
 
@@ -15,9 +17,421 @@ class CgChartView extends StatefulWidget {
       ),
       body: SingleChildScrollView(
         child: Container(
-          padding: const EdgeInsets.all(10.0),
+          padding: const EdgeInsets.all(20.0),
           child: Column(
-            children: const [],
+            children: [
+              //#TEMPLATE chart_line
+              Builder(
+                builder: (context) {
+                  final List<Map> chartData = [
+                    {
+                      "year": 2018,
+                      "sales": 40,
+                    },
+                    {
+                      "year": 2019,
+                      "sales": 90,
+                    },
+                    {
+                      "year": 2020,
+                      "sales": 30,
+                    },
+                    {
+                      "year": 2021,
+                      "sales": 80,
+                    },
+                    {
+                      "year": 2022,
+                      "sales": 90,
+                    }
+                  ];
+
+                  return Container(
+                    color: Theme.of(context).cardColor,
+                    padding: const EdgeInsets.all(12.0),
+                    child: SfCartesianChart(
+                      series: <ChartSeries>[
+                        // Renders line chart
+                        LineSeries<Map, int>(
+                          dataSource: chartData,
+                          xValueMapper: (Map data, _) => data["year"],
+                          yValueMapper: (Map data, _) => data["sales"],
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+              //#END
+              const SizedBox(
+                height: 20.0,
+              ),
+              //#TEMPLATE chart_spline
+              Builder(
+                builder: (context) {
+                  final List<Map> chartData = [
+                    {
+                      "year": 2018,
+                      "sales": 40,
+                    },
+                    {
+                      "year": 2019,
+                      "sales": 90,
+                    },
+                    {
+                      "year": 2020,
+                      "sales": 30,
+                    },
+                    {
+                      "year": 2021,
+                      "sales": 80,
+                    },
+                    {
+                      "year": 2022,
+                      "sales": 90,
+                    }
+                  ];
+
+                  return Container(
+                    color: Theme.of(context).cardColor,
+                    padding: const EdgeInsets.all(12.0),
+                    child: SfCartesianChart(
+                      series: <ChartSeries>[
+                        SplineSeries<Map, int>(
+                          dataSource: chartData,
+                          xValueMapper: (Map data, _) => data["year"],
+                          yValueMapper: (Map data, _) => data["sales"],
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+              //#END
+              const SizedBox(
+                height: 20.0,
+              ),
+              //#TEMPLATE chart_bar_horizontal
+              Builder(
+                builder: (context) {
+                  final List<Map> chartData = [
+                    {
+                      "year": 2018,
+                      "sales": 40,
+                    },
+                    {
+                      "year": 2019,
+                      "sales": 90,
+                    },
+                    {
+                      "year": 2020,
+                      "sales": 30,
+                    },
+                    {
+                      "year": 2021,
+                      "sales": 80,
+                    },
+                    {
+                      "year": 2022,
+                      "sales": 90,
+                    }
+                  ];
+
+                  return Container(
+                    color: Theme.of(context).cardColor,
+                    padding: const EdgeInsets.all(12.0),
+                    child: SfCartesianChart(
+                      series: <ChartSeries>[
+                        BarSeries<Map, int>(
+                          dataSource: chartData,
+                          xValueMapper: (Map data, _) => data["year"],
+                          yValueMapper: (Map data, _) => data["sales"],
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+              //#END
+              const SizedBox(
+                height: 20.0,
+              ),
+              //#TEMPLATE chart_bar_vertical
+              Builder(
+                builder: (context) {
+                  final List<Map> chartData = [
+                    {
+                      "year": 2018,
+                      "sales": 40,
+                    },
+                    {
+                      "year": 2019,
+                      "sales": 90,
+                    },
+                    {
+                      "year": 2020,
+                      "sales": 30,
+                    },
+                    {
+                      "year": 2021,
+                      "sales": 80,
+                    },
+                    {
+                      "year": 2022,
+                      "sales": 90,
+                    }
+                  ];
+
+                  return Container(
+                    color: Theme.of(context).cardColor,
+                    padding: const EdgeInsets.all(12.0),
+                    child: SfCartesianChart(
+                      isTransposed: true,
+                      primaryXAxis: NumericAxis(
+                        decimalPlaces: 0,
+                      ),
+                      series: <ChartSeries>[
+                        BarSeries<Map, int>(
+                          dataSource: chartData,
+                          xValueMapper: (Map data, _) => data["year"],
+                          yValueMapper: (Map data, _) => data["sales"],
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+              //#END
+              const SizedBox(
+                height: 20.0,
+              ),
+              //#TEMPLATE chart_scatter
+              Builder(
+                builder: (context) {
+                  final List<Map> chartData = [
+                    {
+                      "year": 2018,
+                      "sales": 40,
+                    },
+                    {
+                      "year": 2019,
+                      "sales": 90,
+                    },
+                    {
+                      "year": 2020,
+                      "sales": 30,
+                    },
+                    {
+                      "year": 2021,
+                      "sales": 80,
+                    },
+                    {
+                      "year": 2022,
+                      "sales": 90,
+                    }
+                  ];
+
+                  return Container(
+                    color: Theme.of(context).cardColor,
+                    padding: const EdgeInsets.all(12.0),
+                    child: SfCartesianChart(
+                      series: <ChartSeries>[
+                        ScatterSeries<Map, int>(
+                          dataSource: chartData,
+                          xValueMapper: (Map data, _) => data["year"],
+                          yValueMapper: (Map data, _) => data["sales"],
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+              //#END
+              const SizedBox(
+                height: 20.0,
+              ),
+              //#TEMPLATE chart_area
+              Builder(
+                builder: (context) {
+                  final List<Map> chartData = [
+                    {
+                      "year": 2018,
+                      "sales": 40,
+                    },
+                    {
+                      "year": 2019,
+                      "sales": 90,
+                    },
+                    {
+                      "year": 2020,
+                      "sales": 30,
+                    },
+                    {
+                      "year": 2021,
+                      "sales": 80,
+                    },
+                    {
+                      "year": 2022,
+                      "sales": 90,
+                    }
+                  ];
+
+                  return Container(
+                    color: Theme.of(context).cardColor,
+                    padding: const EdgeInsets.all(12.0),
+                    child: SfCartesianChart(
+                      series: <ChartSeries>[
+                        AreaSeries<Map, int>(
+                          dataSource: chartData,
+                          xValueMapper: (Map data, _) => data["year"],
+                          yValueMapper: (Map data, _) => data["sales"],
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+              //#END
+              const SizedBox(
+                height: 20.0,
+              ),
+              //#TEMPLATE chart_bubble
+              Builder(
+                builder: (context) {
+                  final List<Map> chartData = [
+                    {
+                      "year": 2018,
+                      "sales": 40,
+                    },
+                    {
+                      "year": 2019,
+                      "sales": 90,
+                    },
+                    {
+                      "year": 2020,
+                      "sales": 30,
+                    },
+                    {
+                      "year": 2021,
+                      "sales": 80,
+                    },
+                    {
+                      "year": 2022,
+                      "sales": 90,
+                    }
+                  ];
+
+                  return Container(
+                    color: Theme.of(context).cardColor,
+                    padding: const EdgeInsets.all(12.0),
+                    child: SfCartesianChart(
+                      series: <ChartSeries>[
+                        BubbleSeries<Map, int>(
+                          dataSource: chartData,
+                          xValueMapper: (Map data, _) => data["year"],
+                          yValueMapper: (Map data, _) => data["sales"],
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+              //#END
+              const SizedBox(
+                height: 20.0,
+              ),
+              //#TEMPLATE chart_bubble
+              Builder(
+                builder: (context) {
+                  final List<Map> chartData = [
+                    {
+                      "year": 2018,
+                      "sales": 40,
+                    },
+                    {
+                      "year": 2019,
+                      "sales": 90,
+                    },
+                    {
+                      "year": 2020,
+                      "sales": 30,
+                    },
+                    {
+                      "year": 2021,
+                      "sales": 80,
+                    },
+                    {
+                      "year": 2022,
+                      "sales": 90,
+                    }
+                  ];
+
+                  return Container(
+                    color: Theme.of(context).cardColor,
+                    padding: const EdgeInsets.all(12.0),
+                    child: SfCartesianChart(
+                      series: <ChartSeries>[
+                        StepLineSeries<Map, int>(
+                          dataSource: chartData,
+                          xValueMapper: (Map data, _) => data["year"],
+                          yValueMapper: (Map data, _) => data["sales"],
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+              //#END
+              const SizedBox(
+                height: 20.0,
+              ),
+              //#TEMPLATE chart_pie
+              Builder(
+                builder: (context) {
+                  final List<Map> chartData = [
+                    {
+                      "year": "Jan",
+                      "sales": 40,
+                    },
+                    {
+                      "year": "Feb",
+                      "sales": 90,
+                    },
+                    {
+                      "year": "Mar",
+                      "sales": 30,
+                    },
+                    {
+                      "year": "Apr",
+                      "sales": 80,
+                    },
+                    {
+                      "year": "May",
+                      "sales": 90,
+                    }
+                  ];
+
+                  return Container(
+                    color: Theme.of(context).cardColor,
+                    padding: const EdgeInsets.all(12.0),
+                    child: SfCircularChart(
+                      legend: Legend(isVisible: true),
+                      series: <CircularSeries>[
+                        PieSeries<Map, String>(
+                          dataSource: chartData,
+                          dataLabelSettings: const DataLabelSettings(
+                            isVisible: true,
+                          ),
+                          xValueMapper: (Map data, _) => data["year"],
+                          yValueMapper: (Map data, _) => data["sales"],
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+              //#END
+            ],
           ),
         ),
       ),
