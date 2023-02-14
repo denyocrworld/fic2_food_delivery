08bd06ce2479ed372b74c46da70b648a7177b88e
flutterlabz <flutterlabz@gmail.com>
2023-02-15 00:48:26 +0700

    .

--- fix_file_name.dart
+++ fix_file_name.dart
@@ -2,4 +2,9 @@ import 'dart:io';
 
 void main() {
   var dir = Directory("./");
+  var list = dir.listSync(recursive: true);
+  for (var f in list) {
+    if (f is Directory) {
+    } else if (f is File) {}
+  }
 }
